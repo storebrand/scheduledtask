2774f0c854663b3e5e8a91297d9168721cd65636
Kristian Hiim <kristian.hiim@storebrand.no>
2022-02-02 14:16:45 +0100

    Copy implementation from SKAGENGoodies
    - copy most of the implementation of ScheduledTask from SKAGENGoodies,
    so we can work with it on Github.
    - made required modifications so it now builds.

---
+++ scheduledtask-core/src/main/java/com/storebrand/scheduledtask/TableInspector.java
@@ -0,0 +1,429 @@
+package com.storebrand.scheduledtask;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.JDBCType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.storebrand.scheduledtask.internal.util.StringUtils;
+
+/**
+ * Will check and see if a table exists in the database and retrieve the {@link TableColumn}s and Primary keys names so
+ * the calling code can inspect to see if all the fields are in this database.
+ *
+ * @see <a href="https://www.progress.com/blogs/jdbc-tutorial-extracting-database-metadata-via-jdbc-driver">JDBC
+ * Tutorial: Extracting Database Metadata via JDBC</>.
+ */
+public class TableInspector {
+    private static final Logger log = LoggerFactory.getLogger(TableInspector.class);
+    public static final String TABLE_VERSION = "STB_SCHEDULE_TABLE_VERSION";
+    public static final int VALID_VERSION = 1;
+    private final Map<String, TableColumn> _tableColumns;
+    private final Map<String, String> _primaryKeys;
+    private final Map<String, ForeignKey> _foreignKeys;
+    private final String _tableName;
+    private static final String MIGRATION_FILE_NAME = "V20210222_1__Create_initial_tables.sql";
+    private final DataSource _dataSource;
+
+    public TableInspector(DataSource dataSource, String tableName) {
+        _tableName = tableName;
+        _dataSource = dataSource;
+        try (Connection connection = dataSource.getConnection()) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            _tableColumns = extractColumns(metaData, tableName.toUpperCase());
+            _primaryKeys = extractPrimaryKeys(metaData, tableName.toUpperCase());
+            _foreignKeys = extractForeignKeys(metaData, tableName.toUpperCase());
+        }
+        catch (SQLException throwables) {
+            throw new TableValidationException("Unable to retrieve the metadata for the given dataSource. "
+                    + getMigrationLocationMessage(), throwables);
+        }
+    }
+
+    /**
+     * Checks the table version table to see if the database are using the correct {@link #MIGRATION_FILE_NAME} version.
+     *
+     */
+    public int getTableVersion() {
+        String sql = "SELECT * FROM " + TABLE_VERSION;
+
+        try (Connection sqlConnection = _dataSource.getConnection();
+             PreparedStatement pStmt = sqlConnection.prepareStatement(sql);
+             ResultSet result = pStmt.executeQuery()) {
+
+            if (result.first()) {
+                return result.getInt("version");
+            }
+        }
+        catch (SQLException throwables) {
+            throw new RuntimeException(getMigrationLocationMessage(), throwables);
+        }
+        return 0;
+    }
+
+    /**
+     * The amount of column found in the specified table.
+     */
+    public int amountOfColumns() {
+        return _tableColumns.size();
+    }
+
+    /**
+     * Checks if we found the column with the given name
+     */
+    public boolean columnExists(String columnName) {
+        return _tableColumns.containsKey(columnName.toUpperCase());
+    }
+
+    /**
+     * Helper method to quickly check if a column has the specified parameters without verifying the column size.
+     * @param columnName - Name of the column to check
+     * @param isNullable - Check if this column can be set to <b>null</b>
+     * @param dataTypes - The {@link Types} this column should have. Note since there is multiple valid types like
+     *                    {@link Types#VARCHAR} and {@link Types#NVARCHAR} we can supply a list of types it should
+     *                    have one of. Due to different databases may use different types we may need to supply multiple
+     *                    types that should be a valid option
+     */
+    public void validateColumn(String columnName, boolean isNullable, JDBCType... dataTypes) {
+        validateColumn(columnName, null, isNullable, dataTypes);
+    }
+
+    /**
+     * Helper method to quickly check if a column has the specified parameters.
+     *
+     * @param columnName - Name of the column to check
+     * @param minColumnSize - The size of the column. Usually used with columns like {@link Types#VARCHAR}.
+     *                        If set to null this check will be skipped.
+     * @param isNullable - Check if this column can be set to <b>null</b>
+     * @param dataTypes - The {@link Types} this column should have. Note since there is multiple valid types like
+     *                    {@link Types#VARCHAR} and {@link Types#NVARCHAR} we can supply a list of types it should
+     *                    have one of. Due to different databases may use different types we may need to supply multiple
+     *                    types that should be a valid option
+     */
+    @SuppressWarnings({"BooleanExpressionComplexity"})
+    public void validateColumn(String columnName, Integer minColumnSize, boolean isNullable, JDBCType... dataTypes) {
+        // ?: Did we find the table column?
+        if (!columnExists(columnName)) {
+            // E-> No, column not found
+            throw new TableValidationException("Table column '" + columnName + "' "
+                    + "where not found in the table '" + _tableName + "' " + getMigrationLocationMessage());
+        }
+        // E-> Yes we did find the column
+        List<JDBCType> validTypes = Arrays.asList(dataTypes);
+        // :? Validate that the column has the specified parameters
+        TableColumn columnToCheck = _tableColumns.get(columnName.toUpperCase());
+        if (validTypes.contains(columnToCheck.getDataType())
+                // if columnSize == null we should not validate this column:
+                && (minColumnSize == null || columnToCheck.getColumnSize() >= minColumnSize)
+                && columnToCheck.isNullable().orElse(false) == isNullable) {
+            // -> Yes, this column has the correct parameters
+            return;
+        }
+
+        // E-> No the table column has not the correct size
+        log.info("Table '" + _tableName + "' column '" + columnName + "' where invalid, found: " + columnToCheck
+                + ", expected to find one of dataTypes:'" + validTypes + "', minColumnSize:'" + minColumnSize + "'"
+                + ", isNullable:'" + isNullable + "'");
+        String errorMessage = "Table column '" + columnName + "' in table '" + _tableName + "'"
+                + " should have one of the dataTypes '" + validTypes + "'"
+                + " and should be set to " + (isNullable ? "'NULL'" : "'NOT NULL'.");
+        errorMessage = errorMessage + "Column size should be at least '" + minColumnSize + "'";
+        errorMessage = errorMessage + " " + getMigrationLocationMessage();
+        throw new TableValidationException(errorMessage);
+    }
+
+    /**
+     * Helper method to check if a table has a primary key with a specific name.
+     */
+    public boolean hasPrimaryKey(String columnName, String primaryKeyName) {
+        // ?: Does this column have a private key at all
+        if (!_primaryKeys.containsKey(columnName.toUpperCase())) {
+            // -> No, this column does not have any primary keys
+            return false;
+        }
+
+        // E-> Yes we have a primary key set but check if it has the same name:
+        return _primaryKeys.get(columnName).equalsIgnoreCase(primaryKeyName);
+    }
+
+    /**
+     * Check if a given column has a foreign key set.
+     *
+     * @param columnName - The user column that has this foreign key set. IE this column has a foreign key that is
+     * another table columns primary key
+     * @param keyOwnerTableName - The table name of the table that is the owner of the key. IE this has this set as a
+     * primary key and 'OWNS' this field.
+     * @param keyOwnerColumnName - The column name that is the owner of the key. IE this has a primary key and 'OWNS' the
+     * field.
+     */
+    public boolean hasForeignKey(String columnName, String keyOwnerTableName, String keyOwnerColumnName) {
+        // ?: Does this column have a private key at all
+        if (!_foreignKeys.containsKey(columnName.toUpperCase())) {
+            // -> No, this column does not have any primary keys
+            return false;
+        }
+
+        // E-> Yes we have a primary key set but check if it has the same name:
+        return _foreignKeys.get(columnName.toUpperCase()).getPrimaryKeyOwnerTable().equalsIgnoreCase(keyOwnerTableName)
+                && _foreignKeys.get(columnName.toUpperCase()).getPrimaryKeyOwnerColumn().equalsIgnoreCase(keyOwnerColumnName);
+    }
+
+    /**
+     * Returns the location of the migrationfile containing the creation of the required tables.
+     */
+    public final String getMigrationFileLocation() {
+        URL codeSourceLocation = this.getClass().getProtectionDomain().getCodeSource().getLocation();
+        return codeSourceLocation + "com/skagenfondene/skagengoodies/scheduler/" + MIGRATION_FILE_NAME;
+    }
+
+    public final String getMigrationLocationMessage() {
+        return "Make sure you are using the migration script "
+                + "'" + getMigrationFileLocation() + "' to create the required tables";
+    }
+
+    /**
+     * Retrieve the map containing the information on the table columns.
+     */
+    public Map<String, TableColumn> getTableColumns() {
+        return Collections.unmodifiableMap(_tableColumns);
+    }
+
+    /**
+     * Retrieve the map containing the primary keys for columns
+     */
+    public Map<String, String> getPrimaryKeys() {
+        return Collections.unmodifiableMap(_primaryKeys);
+    }
+
+    private Map<String, TableColumn> extractColumns(DatabaseMetaData metaData, String tableName) {
+        // Get the table columns
+        try (ResultSet columns = metaData.getColumns(null, null, tableName, null)) {
+
+            Map<String, TableColumn> tableColumns = new HashMap<>();
+            // Loop through all columns an extract the sugar
+            while (columns.next()) {
+                TableColumn tableColumn = new TableColumn(
+                        columns.getString("COLUMN_NAME"),
+                        columns.getInt("DATA_TYPE"),
+                        columns.getString("TYPE_NAME"),
+                        columns.getInt("COLUMN_SIZE"),
+                        columns.getString("IS_NULLABLE"),
+                        columns.getString("IS_AUTOINCREMENT")
+                );
+                tableColumns.put(tableColumn.columnName, tableColumn);
+            }
+
+            return tableColumns;
+        }
+        catch (SQLException throwable) {
+            throw new TableValidationException("Unable to retrieve the columns for the table [" + tableName + "]",
+                    throwable);
+        }
+    }
+
+    private Map<String, String> extractPrimaryKeys(DatabaseMetaData metaData, String tableName) {
+        try (ResultSet keys = metaData.getPrimaryKeys(null, null, tableName.toUpperCase())) {
+
+            Map<String, String> primaryKeys = new HashMap<>();
+            while (keys.next()) {
+                String columnName = keys.getString("COLUMN_NAME");
+                String primaryKeyName = keys.getString("PK_NAME");
+                primaryKeys.put(columnName, primaryKeyName);
+            }
+            return primaryKeys;
+        }
+        catch (SQLException throwable) {
+            throw new TableValidationException("Unable to retrieve the primary keys for the table [" + tableName + "]",
+                    throwable);
+        }
+    }
+
+    private Map<String, ForeignKey> extractForeignKeys(DatabaseMetaData metaData, String tableName) {
+        try (ResultSet keys = metaData.getImportedKeys(null, null, tableName.toUpperCase())) {
+
+            Map<String, ForeignKey> foreignKeys = new HashMap<>();
+            while (keys.next()) {
+                String columnName = keys.getString("FKCOLUMN_NAME");
+                ForeignKey foreignKey = new ForeignKey(
+                        keys.getString("PKTABLE_NAME"),
+                        keys.getString("PKCOLUMN_NAME"),
+                        keys.getString("FKTABLE_NAME"),
+                        keys.getString("FKCOLUMN_NAME")
+                );
+                foreignKeys.put(columnName, foreignKey);
+            }
+            return foreignKeys;
+        }
+        catch (SQLException throwable) {
+            throw new TableValidationException("Unable to retrieve the foreign keys for the table [" + tableName + "]",
+                    throwable);
+        }
+    }
+
+    /**
+     * Class that holds the column information
+     */
+    public static class TableColumn {
+        private final String columnName;
+        private final int dataType;
+        private final String typeName;
+        private final int columnSize;
+        private final String nullable;
+        private final String autoIncrement;
+
+        public TableColumn(String columnName, int dataType, String typeName, int columnSize, String isNullable,
+                String isAutoIncrement) {
+            this.columnName = columnName;
+            this.typeName = typeName;
+            this.dataType = dataType;
+            this.columnSize = columnSize;
+            this.nullable = isNullable;
+            this.autoIncrement = isAutoIncrement;
+        }
+
+        public String getColumnName() {
+            return columnName;
+        }
+
+        /**
+         * The SQL type from {@link Types}
+         */
+        public String getTypeName() {
+            return typeName;
+        }
+
+        /**
+         * Get the data source dependent type name of the column in the db it self.
+         */
+        public JDBCType getDataType() {
+            return JDBCType.valueOf(dataType);
+        }
+
+        public int getColumnSize() {
+            return columnSize;
+        }
+
+        public Optional<Boolean> isNullable() {
+            // ?: Is isNullable set
+            if (!StringUtils.hasText(nullable)) {
+                // -> No, it is unknown so return optional empty
+                return Optional.empty();
+            }
+
+            // E-> yes we have either yes or no here
+            if ("no".equalsIgnoreCase(nullable)) {
+                // -> No, it is not a nullable field.
+                return Optional.of(false);
+            }
+
+            // E-> Yes this field is nullable
+            return Optional.of(true);
+        }
+
+        public Optional<Boolean> isAutoIncrement() {
+            // ?: Is autoIncrement set
+            if (!StringUtils.hasText(autoIncrement)) {
+                // -> No, it is unknown so return optional empty
+                return Optional.empty();
+            }
+
+            // E-> yes we have either yes or no here
+            if ("no".equalsIgnoreCase(autoIncrement)) {
+                // -> No, it is not a auto incremented field.
+                return Optional.of(false);
+            }
+
+            // E-> Yes, this field is autoIncremental
+            return Optional.of(true);
+        }
+
+        @Override
+        public String toString() {
+            return "{ columnName:" + columnName + ", "
+                    + "dataType:" + dataType + ", "
+                    + "typeName:" + typeName + ", "
+                    + "columnSize:" + columnSize + ", "
+                    + "isNullable:" + nullable + ", "
+                    + "isAutoIncrement:" + autoIncrement + "}";
+        }
+    }
+
+    /**
+     * Exception thrown when the table we try to validate either can't be found or there is some differences on what we
+     * expect and what it has been set up to use.
+     */
+    public static class TableValidationException extends RuntimeException {
+        public TableValidationException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public TableValidationException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Holds the Primary key owner tableName and column. Note by Owner it means the table that actually has that column
+     * as a primary key. It also holds the foreign key tableName and column, by this it means the tableName and
+     * columnName of the table that actually uses the the field as a foreign key.
+     *
+     */
+    private static class ForeignKey {
+        private final String primaryKeyOwnerTable;
+        private final String primaryKeyOwnerColumn;
+        private final String foreignKeyTable;
+        private final String foreignKeyColumn;
+
+        ForeignKey(String primaryKeyOwnerTable, String primaryKeyOwnerColumn, String foreignKeyTable,
+                String foreignKeyColumn) {
+            this.primaryKeyOwnerTable = primaryKeyOwnerTable;
+            this.primaryKeyOwnerColumn = primaryKeyOwnerColumn;
+            this.foreignKeyTable = foreignKeyTable;
+            this.foreignKeyColumn = foreignKeyColumn;
+        }
+
+        /**
+         * Name of the table that has this as a primary key, IE the owner of the primary key.
+         */
+        public String getPrimaryKeyOwnerTable() {
+            return primaryKeyOwnerTable;
+        }
+
+        /**
+         * Name of the column that has this as a primary key, IE the owner of the primary key.
+         */
+        public String getPrimaryKeyOwnerColumn() {
+            return primaryKeyOwnerColumn;
+        }
+
+        /**
+         * Name of the table that uses this key. IE this table B uses the other table A's Primary key as ForeignKey.
+         */
+        public String getForeignKeyTable() {
+            return foreignKeyTable;
+        }
+
+        /**
+         * Name of the column that uses this key. IE this table B uses the other table A's Primary key as ForeignKey.
+         */
+        public String getForeignKeyColumn() {
+            return foreignKeyColumn;
+        }
+    }
+}
+
---
+++ scheduledtask-core/src/main/java/com/storebrand/scheduledtask/V20210222_1__Create_initial_tables.sql
@@ -0,0 +1,92 @@
+--------------------------------------------------
+-- stb_schedule_master_locker
+--------------------------------------------------
+-- Table for keeping track of the ScheduledTask master locks
+-- Column lock_name: the name of the scheduler
+-- Column node_name: node of the server that currently has the lock
+-- Column lock_taken_time: when this lock where last taken
+-- Column lock_last_updated_time: used by the master node to keep the lock
+--
+-- The columns lock_taken_time and lock_last_updated_time is used to keep track on the master node
+-- to see if he currently is actively keeping the lock and how long he has kept it.
+CREATE TABLE stb_schedule_master_locker (
+    lock_name VARCHAR NOT NULL,
+    node_name VARCHAR NOT NULL,
+    lock_taken_time datetime2 NOT NULL,
+    lock_last_updated_time datetime2 NOT NULL,
+    CONSTRAINT PK_lock_name PRIMARY KEY (lock_name)
+);
+
+--------------------------------------------------
+-- stb_schedule_table_version
+--------------------------------------------------
+-- This table holds a counter on what version this file 'Create_initial_tables' are. It is used during starup of the
+-- scheduler to verify we have the same version in the service as goodies expects.
+CREATE TABLE stb_schedule_table_version (
+    version int NOT NULL
+);
+
+-- If changes are done to these tables this value should crease by one.
+INSERT INTO stb_schedule_table_version (version) VALUES (1);
+
+--------------------------------------------------
+-- stb_schedule
+--------------------------------------------------
+-- Table for keeping track of the ScheduledTask's
+-- Column schedule_name: the name of the schedule
+-- Column is_active: flag that informs if this schedule is active (IE is running or paused)
+-- Column run_once: flag that informs that this schedule should run immediately regardless of next_run
+-- Column cron_expression: When null the default coded in the java file will be used. if set then tis is the override
+-- Column next_run: timestamp on when the schedule should be running next time
+-- Column last_updated: Timestamp when this row where last updated. IE when the last run where triggered.
+--
+-- Note the last_updated may be set even if the is_active is false. This means the execution of the schedule is
+-- deactivated but it will be doing it's normal schedule "looping"
+CREATE TABLE stb_schedule (
+    schedule_name VARCHAR NOT NULL,
+    is_active BIT NOT NULL,
+    run_once BIT NOT NULL,
+    cron_expression VARCHAR NULL,
+    next_run datetime2 NOT NULL,
+    last_updated datetime2 NOT NULL,
+    CONSTRAINT PK_schedule_name PRIMARY KEY (schedule_name)
+);
+
+--------------------------------------------------
+-- stb_schedule_run
+--------------------------------------------------
+-- Table for scheduleRun. This has the run history the schedules
+-- Column schedule_name: the name of the schedule
+-- Column instance_id: Instance for the run. = runTime + "-" + nodeName
+-- Column run_start: When this schedule where started.
+-- Column status state of the schedule run, should be one of ScheduleTaskImpl.State STARTªED/FAILED/DISPATCHED/DONE
+-- Column status_msg: Some informing text that is connected to the state.
+-- Column status_throwable: Can only be set on STATUS = FAILED and can contain a throwable
+-- Column status_time: When this schedule state where set.
+CREATE TABLE stb_schedule_run (
+    instance_id VARCHAR NOT NULL,
+    schedule_name VARCHAR NOT NULL,
+    run_start DATETIME2 NOT NULL,
+    status VARCHAR NULL,
+    status_msg VARCHAR NULL,
+    status_throwable VARCHAR NULL,
+    status_time DATETIME2 NOT NULL,
+    CONSTRAINT PK_instance_id PRIMARY KEY (instance_id)
+);
+
+--------------------------------------------------
+-- stb_schedule_log_entry
+--------------------------------------------------
+-- Table for scheduleRunLogs storing the logs (if any) for a scheduleRun
+-- Column: instance_id: Instance for the run. = runTime + "-" + nodeName. Foreign Key is from the scheduleRun table
+-- Column: log_msg: message that is logged for the run.
+-- Column: log_throwable: If set contains a trowable in addition to the log_msg
+-- Column: log_time: timestamp on when this log where recorded.
+
+CREATE TABLE stb_schedule_log_entry (
+    instance_id VARCHAR NOT NULL,
+    log_msg VARCHAR NOT NULL,
+    log_throwable VARCHAR NULL,
+    log_time datetime2 NOT NULL,
+    CONSTRAINT FK_instance_id FOREIGN KEY (instance_id) REFERENCES stb_schedule_run (instance_id)
+);
