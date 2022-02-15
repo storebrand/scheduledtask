54f95c899f1a305a332b8e85171b30c8c22135d2
Kristian Hiim <kristian.hiim@storebrand.no>
2022-02-15 12:03:50 +0100

    Spring wiring for scheduled tasks
    - a simple EnableScheduledTasks annotation that wires up
    ScheduledTaskService in the Spring context.
    - a factory bean that handles providing an actual implementation, and
    shutting down the service when the spring context is shutting down.
    - option to use default repository implementations or provide one
    through Spring.

---
+++ scheduledtask-spring/src/main/java/com/storebrand/scheduledtask/spring/EnableScheduledTasks.java
@@ -0,0 +1,56 @@
+package com.storebrand.scheduledtask.spring;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.context.annotation.Role;
+import org.springframework.core.type.AnnotationMetadata;
+
+import com.storebrand.scheduledtask.spring.EnableScheduledTasks.ScheduledTaskBeanRegistration;
+
+/**
+ * Spring annotation for enabling scheduled tasks. Sets up the {@link ScheduledTaskServiceFactory} that will provide an
+ * instance of the {@link com.storebrand.scheduledtask.ScheduledTaskService}.
+ * <p>
+ * The factory will by default assume that we want to use the standard implementation of repositories provided by the
+ * scheduledtask-db-sql package. These require a {@link javax.sql.DataSource} bean in the spring context. If there are
+ * multiple beans of this type then one must be selected as a primary bean.
+ * <p>
+ * If you do not wish to use the standard implementation of repositories you can override this behaviour by providing
+ * implementations of {@link com.storebrand.scheduledtask.db.ScheduledTaskRepository} and
+ * {@link com.storebrand.scheduledtask.db.MasterLockRepository} in the Spring context.
+ *
+ * @author Kristian Hiim
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import({ ScheduledTaskBeanRegistration.class })
+public @interface EnableScheduledTasks {
+
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    class ScheduledTaskBeanRegistration implements ImportBeanDefinitionRegistrar {
+        private static final Logger log = LoggerFactory.getLogger(ScheduledTaskBeanRegistration.class);
+
+        @Override
+        public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
+                BeanDefinitionRegistry registry) {
+            // :: Register ScheduledTaskServiceFactory bean
+            log.info("Enabling Scheduled tasks - Registering Spring bean definition for "
+                    + ScheduledTaskServiceFactory.class.getSimpleName());
+            BeanDefinition beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(ScheduledTaskServiceFactory.class)
+                    .getBeanDefinition();
+            registry.registerBeanDefinition(
+                    AnnotationBeanNameGenerator.INSTANCE.generateBeanName(beanDefinition, registry), beanDefinition);
+        }
+    }
+}
---
+++ scheduledtask-spring/src/main/java/com/storebrand/scheduledtask/spring/ScheduledTaskServiceFactory.java
@@ -0,0 +1,112 @@
+package com.storebrand.scheduledtask.spring;
+
+import java.time.Clock;
+import java.util.List;
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import com.storebrand.scheduledtask.ScheduledTaskService;
+import com.storebrand.scheduledtask.ScheduledTaskServiceImpl;
+import com.storebrand.scheduledtask.db.MasterLockRepository;
+import com.storebrand.scheduledtask.db.ScheduledTaskRepository;
+import com.storebrand.scheduledtask.db.sql.MasterLockSqlRepository;
+import com.storebrand.scheduledtask.db.sql.ScheduledTaskSqlRepository;
+
+/**
+ * Factory bean that will create an implementation of {@link ScheduledTaskService}. This will also handle shutting down
+ * the service when the application context is shutting down.
+ * <p>
+ * {@link ScheduledTaskServiceImpl} requires both a {@link ScheduledTaskRepository} and a {@link MasterLockRepository}.
+ * The factory will try to inject these as optional dependencies. If they are found it will use them.
+ * <p>
+ * If there are no repositories present the factory will look for a {@link DataSource} in the Spring context, and use
+ * that to create the default implementations of the repositories. Note that default implementations created here will
+ * not be available through the Spring context, as they are only meant for internal use. Also, if there are multiple
+ * datasources available in the Spring context the primary bean will be used. If there are no primary bean
+ * initialization will fail.
+ * <p>
+ * As the service and repositories also require a {@link Clock} the factory will look for that in the Spring context as
+ * well. If it is not found {@link Clock#systemDefaultZone()} will be used.
+ *
+ * @author Kristian Hiim
+ */
+public class ScheduledTaskServiceFactory extends AbstractFactoryBean<ScheduledTaskService>
+        implements ApplicationContextAware {
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTaskServiceFactory.class);
+
+    @Inject
+    private Optional<MasterLockRepository> _masterLockRepository;
+
+    @Inject
+    private Optional<ScheduledTaskRepository> _scheduledTaskRepository;
+
+    @Inject
+    private Optional<List<DataSource>> _dataSources;
+
+    @Inject
+    private Optional<Clock> _clock;
+
+    private ApplicationContext _applicationContext;
+
+    public ScheduledTaskServiceFactory() {
+        setSingleton(true);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        _applicationContext = applicationContext;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return ScheduledTaskServiceImpl.class;
+    }
+
+    @Override
+    protected ScheduledTaskService createInstance() {
+        // Use Clock from Spring context if it is present, or use system default zone.
+        final Clock clock = _clock.orElseGet(Clock::systemDefaultZone);
+
+        // Attempt to detect if we have a single DataSource in Spring context.
+        // dataSource will be empty if there are 0 or more than 1 datasource.
+        final Optional<DataSource> dataSource = _dataSources.isPresent() && _dataSources.get().size() == 1
+                ? Optional.of(_dataSources.get().get(0))
+                : Optional.empty();
+
+        // Get MasterLockRepository from Spring context, or create a default implementation if not present.
+        MasterLockRepository masterLockRepository = _masterLockRepository.orElseGet(() -> {
+            log.info(MasterLockRepository.class.getSimpleName() + " not found in Spring context - Creating default implementation "
+                    + MasterLockSqlRepository.class.getName());
+            return new MasterLockSqlRepository(
+                    dataSource.orElseGet(() -> _applicationContext.getBean(DataSource.class)),
+                    clock);
+        });
+
+        // Get ScheduledTaskRepository from Spring context, or create a default implementation if not present.
+        ScheduledTaskRepository scheduledTaskRepository = _scheduledTaskRepository.orElseGet(() -> {
+            log.info(ScheduledTaskRepository.class.getSimpleName() + " not found in Spring context - Creating default implementation "
+                    + ScheduledTaskSqlRepository.class.getName());
+            return new ScheduledTaskSqlRepository(
+                    dataSource.orElseGet(() -> _applicationContext.getBean(DataSource.class)),
+                    clock);
+        });
+
+        return new ScheduledTaskServiceImpl(scheduledTaskRepository, masterLockRepository, clock);
+    }
+
+    @Override
+    protected void destroyInstance(ScheduledTaskService instance) {
+        if (instance != null) {
+            instance.close();
+        }
+    }
+}
